2c4de578997a2f35d9a07a2818b1c879a8a9efca
gfdgd_xi <3025613752@qq.com>
2023-07-09 10:22:02 +0800

    优化

--- mirror.sh
+++ mirror.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 cat > /etc/apt/mirror.list <<EOF
+############# config ##################
 set base_path    /waydroid-deb
 set nthreads     20
 set _tilde 0
+############# end config ##############
 deb http://repo.waydro.id/ jammy main
+deb http://repo.waydro.id/ focal main
+deb http://repo.waydro.id/ bookworm main
+deb http://repo.waydro.id/ bullseye main
 #clean https://repo.waydro.id/
 EOF
