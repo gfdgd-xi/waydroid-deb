2cba32a8a55f9a3bf8b9a4b13f6647ec91c19e39
gfdgd_xi <3025613752@qq.com>
2023-07-09 10:16:29 +0800

    try again

--- mirror.sh
+++ mirror.sh
@@ -4,5 +4,5 @@ set base_path    /waydroid-deb
 set nthreads     20
 set _tilde 0
 deb [signed-by=/usr/share/keyrings/waydroid.gpg] https://repo.waydro.id/ jammy main
-clean https://repo.waydro.id/
+#clean https://repo.waydro.id/
 EOF
