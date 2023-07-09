a3c37a96ebb2f1b6901de083dc2198de000fe52d
gfdgd_xi <3025613752@qq.com>
2023-07-09 10:18:42 +0800

    优化

--- mirror.sh
+++ mirror.sh
@@ -3,6 +3,6 @@ cat > /etc/apt/mirror.list <<EOF
 set base_path    /waydroid-deb
 set nthreads     20
 set _tilde 0
-deb [signed-by=/usr/share/keyrings/waydroid.gpg] https://repo.waydro.id/ jammy main
+deb http://repo.waydro.id/ jammy main
 #clean https://repo.waydro.id/
 EOF
